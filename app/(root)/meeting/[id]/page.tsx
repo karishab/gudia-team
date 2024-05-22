9b09cb789bd66c6e8f34aaccdb7f554bd6461cc6
Salar Shabahang <karishab86@gmail.com>
2024-05-22 20:19:26 +0200

    aa

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Meeting = ({ params }: { params: { id: string } }) => { // aw id haman navi id zirmajmooay meeting foldera
+const Meeting = ({ params }: { params: { id: string } }) => { // aw id haman  [Id]navi id zirmajmooay meeting foldera
     return (
         <div>MeetingRoom #{params.id}</div>
     )
