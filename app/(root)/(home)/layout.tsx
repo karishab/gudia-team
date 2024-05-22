934bfeb53777950725eaa3a1c35c322bce9504d6
Salar Shabahang <karishab86@gmail.com>
2024-05-22 20:24:30 +0200

    bb

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react'
+
+const HomeLayout = ({ children }: { children: ReactNode }) => {
+    return (
+        <main>
+            Navbar
+            {children}
+            Footer
+        </main>
+    );
+}
+
+export default HomeLayout
