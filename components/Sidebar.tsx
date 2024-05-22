bf4340411b773b416c1111d30f97047b867034b9
Salar Shabahang <karishab86@gmail.com>
2024-05-22 20:45:26 +0200

    chenge home sidebar component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col
+    justify-between p-6 pt-28 bg-primary text-white max-sm:hidden lg:w-[264px]'>
+        <div className='flex flex- flex-col gap-6'>
+
+        </div>
+    </section>
+  )
+}
+
+export default Sidebar
