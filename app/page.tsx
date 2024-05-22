452854f025d2aa50427274ad0cc5960d431b7423
Salar Shabahang <karishab86@gmail.com>
2024-05-22 19:46:17 +0200

    start project

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-"use client";
-import { useQuery } from "convex/react";
-import { api } from "../convex/_generated/api";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  const tasks = useQuery(api.tasks.get);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
-    </main>
+    <div>
+      <Button>Clicke mee</Button>
+    </div>
   );
 }
 
