2f10d6b4f615503bd5831b235e137f0317ce6e25
Salar Shabahang <karishab86@gmail.com>
2024-11-22 14:11:01 +0100

    chenged public

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import { RiHomeOfficeFill } from "react-icons/ri";
+
 
 const Sidebar = () => {
-  return (
-    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col
+    return (
+        <section className='sticky left-0 top-0 flex h-screen w-fit flex-col
     justify-between p-6 pt-28 bg-primary text-white max-sm:hidden lg:w-[264px]'>
-        <div className='flex flex- flex-col gap-6'>
+            <div className='flex flex- flex-col gap-6'>
+                <div className='flex items-center gap-4'>
+                    <RiHomeOfficeFill />
+                    <a href='/' className='text-lg font-bold'>
+                        Home</a>
+                </div>
+                <div className='flex items-center gap-4'>
+                    <a href='/upcoming' className='text-lg font-bold'>Upcoming</a>
+                </div>
+                <div className='flex items-center gap-4'>
 
-        </div>
-    </section>
-  )
+                    <a href='/previous' className='text-lg font-bold'>Previous</a>
+                </div>
+                <div className='flex items-center gap-4'>
+
+                    <a href='/recordings' className='text-lg font-bold'>Recordings</a>
+                </div>
+                <div className='flex items-center gap-4'>
+                    <a href='/personal-room' className='text-lg font-bold'>Personal Room</a>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Sidebar
