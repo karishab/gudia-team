e324b4e72fad790668fcd4d28b26b82328921bfb
Salar Shabahang <karishab86@gmail.com>
2024-11-22 15:21:52 +0100

    Navbar-chnded

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,13 +2,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-
 import { sidebarLinks } from '@/constants';
 import { cn } from '@/lib/utils';
-
 const Sidebar = () => {
     const pathname = usePathname();
-
     return (
         <section className="sticky left-0 top-0 flex h-screen w-fit flex-col  justify-between  bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
             <div className="flex flex-1 flex-col gap-6">
